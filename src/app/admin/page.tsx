4eba3dce4f09521ee5f6530536983524bd620330
BAM <bsharamin12@gmail.com>
2025-04-26 18:16:15 +0300

    pass rendered server elements to layout builder

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,20 @@
-"use server";
-import React from "react";
 import LayoutBuilder from "@/components/LayoutBuilder";
+import RenderServerElements from "@/components/LayoutBuilder/RenderServerElements";
+import { NextPageSearchParams } from "@/components/LayoutBuilder/types";
 
-const page = () => {
+const Page = ({
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: NextPageSearchParams;
+}) => {
   return (
     <div>
-      <LayoutBuilder></LayoutBuilder>
+      <LayoutBuilder>
+        <RenderServerElements searchParams={searchParams} />
+      </LayoutBuilder>
     </div>
   );
 };
 
-export default page;
+export default Page;
