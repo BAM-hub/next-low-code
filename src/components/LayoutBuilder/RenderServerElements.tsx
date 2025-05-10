81eee889506c2dfabbdee6fa53687a7d8ab13564
BAM <bsharamin12@gmail.com>
2025-05-11 00:47:34 +0300

    WIP render server elements

--- src/components/LayoutBuilder/RenderServerElements.tsx
+++ src/components/LayoutBuilder/RenderServerElements.tsx
@@ -3,24 +3,31 @@
 import { getComponents } from "@/app/services/components";
 import { NextPageSearchParams } from "./types";
 
+const serverComponentsPathsJson = await getComponents(true);
+
+const serverComponents = await Promise.all(
+  serverComponentsPathsJson.map(async (path) => {
+    const file = await import(`../../components/serverElements/${path}`);
+    return file.default;
+  })
+);
+
+export async function addServerElement(key: number) {
+  const target = serverComponents[key];
+  if (target) {
+    serverComponents.push(target);
+  }
+}
+
 const RenderServerElements = async (props: {
   searchParams: NextPageSearchParams;
 }) => {
-  const serverComponentsPathsJson = await getComponents(true);
-
-  const serverComponents = await Promise.all(
-    serverComponentsPathsJson.map(async (path) => {
-      const file = await import(`../../components/serverElements/${path}`);
-      return file.default;
-    })
-  );
-
   if (!serverComponents.length) return null;
 
   return (
     <>
       {serverComponents.map((Component, index) => {
-        return <Component key={Math.random()} />;
+        return <Component key={index} />;
       })}
     </>
   );
